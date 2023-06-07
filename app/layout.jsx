c2a6a95373259a1406be599b5be8fe10f0263b7f
jonh_nik@windowslive.com <john_nik@windowslive.com>
2023-06-07 15:11:03 +0300

    Sec commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,17 @@
-
 export const metadata = {
-    title: 'Keeper',
-    description : 'Store Notes while online'
-}
-
+  title: "",
+  description: "",
+};
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-    <body>
-        <div className="main">
-        </div>
-        <main className="app">
-        {children}
-        </main>
-    </body>
+      <body>
+        <div className="main"></div>
+        <main className="app">{children}</main>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
